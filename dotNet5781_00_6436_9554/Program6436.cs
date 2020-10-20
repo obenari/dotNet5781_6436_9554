86050ae001af67f1ef8e7f6f750384e13fabaecf
obenari <obenari@g.jct.ac.il>
2020-10-20 20:39:52 +0300

    update program

---
+++ dotNet5781_00_6436_9554/Program6436.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_6436_9554
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome6436();
+            Welcome9554();
+            Console.ReadKey();
+            //
+            //************
+
+        }
+        static partial void Welcome9554();
+        private static void Welcome6436()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
+/*Enter your name:
+dani
+dani, welcome to my first console application
+*/
