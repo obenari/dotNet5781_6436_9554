0fa832921681d86aebacd262d2964ac135655a7b
Aviyaush5050@gmail.com <Aviyaush5050@gmail.com>
2020-10-20 21:00:05 +0300

    a

---
+++ dotNet5781_00_6436_9554/Program9554.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_6436_9554
+{
+    partial class Program
+    {
+        static partial void Welcome9554()
+        {
+            Console.WriteLine("I am also here");
+        }
+
+    }
+}
