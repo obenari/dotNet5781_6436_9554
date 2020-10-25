4004ea2a51b13dbb73b6ce5e24c653a4eb5eb1ee
Aviyaush5050@gmail.com <Aviyaush5050@gmail.com>
2020-10-25 21:09:34 +0200

    aaaaaaa

--- dotNet5781_6436_9554/dotNet5781_01_6436_9554/Properties/AssemblyInfo.cs
+++ dotNet5781_6436_9554/dotNet5781_01_6436_9554/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("78aed9bf-037f-4524-938f-f8b1d91c9893")]
+[assembly: Guid("4af2805c-4900-4a94-97b3-4d8a46d8aa43")]
 
 // Version information for an assembly consists of the following four values:
 //
